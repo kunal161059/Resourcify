1853fae0ad6d227713698ad1437fe924bb046813
kunal161059 <kunalraghu1c001@gmail.com>
2025-02-07 13:50:33 +0530

    Update ESLint config and fix Next.js version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['your-image-domain.com'], // Add your image domains here
+  }
 }
 
 module.exports = nextConfig 
